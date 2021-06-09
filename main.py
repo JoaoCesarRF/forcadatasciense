68d4f3725b90d7aa70067129207b5590863926be
Joao <joaocesar.rizzatto85@gmail.com>
2021-06-09 12:02:47 -0300

    logica finalizada

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#-*- coding: utf-8 -*-
 import random
 import unidecode
 
@@ -7,19 +8,70 @@ def pegar_palavra():
         arq = (palavras.readlines())
         banco =[cada.rstrip('\n') for cada in arq]
         return banco[random.randint(0,len(banco)-1)]
+banco=['''
+>>>>>>>>>>Hangman<<<<<<<<<<
++---+
+|   |
+    |
+    |
+    |
+    |
+=========''', '''
++---+
+|   |
+O   |
+    |
+    |
+    |
+=========''', '''
++---+
+|   |
+O   |
+|   |
+    |
+    |
+=========''', '''
+ +---+
+ |   |
+ O   |
+/|   |
+     |
+     |
+=========''', '''
+ +---+
+ |   |
+ O   |
+/|\  |
+     |
+     |
+=========''', '''
+ +---+
+ |   |
+ O   |
+/|\  |
+/    |
+     |
+=========''', '''
+ +---+
+ |   |
+ O   |
+/|\  |
+/ \  |
+     |
+=========''']
 
 if __name__ == '__main__':
     original = pegar_palavra()
     pal = unidecode.unidecode(original)
     palavra=[letra.upper() for letra in pal]
-    print("".join(palavra).center(15))
+    #print("".join(palavra).center(15))
     qnts_letras=len(palavra)
     letras_ja_inseridas = []
     linha = ['_' for n in range(qnts_letras)]
-
+    acertos=0
+    erros=0
     while True:
-        print('\nJogo da Forca'.center(20))
-        print("".join(palavra).center(15))
+        print(banco[erros])
         print(f'{"".join(linha)}\n {qnts_letras:^} letras')
         print('Letras ja inseridas \n'," ".join(letras_ja_inseridas))
         letra = input('Digite uma letra').upper()
@@ -34,6 +86,18 @@ if __name__ == '__main__':
         for i in range(qnts_letras):
             if palavra[i] == letra:
                 linha[i]=letra
+        if letra in palavra:
+            acertos += 1
+        else:
+            erros += 1
+
+        print(erros)
+        if acertos == qnts_letras:
+            print(f'\nPARABENS !!!!\na palavra e "{original}"')
+            break
+        elif erros == 6:
+            print(f'MMMOOOORRRRRRREEEEEEEEEEUUUUUU \n {banco[erros]}')
+            break
 
 
 
